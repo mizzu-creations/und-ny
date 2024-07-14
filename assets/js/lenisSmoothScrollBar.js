4175a32b829dd7fb5fb5d0806c0c089e7fe49c73
mizzu-creations <mizzu.creations@gmail.com>
2024-07-14 14:55:15 +0900

    Setting : Lenis scroll 적용

---
+++ assets/js/lenisSmoothScrollBar.js
@@ -0,0 +1,26 @@
+export const lenisSmoothScrollBar = () => {
+  const lenis = new Lenis();
+
+  lenis.on("scroll", (e) => {
+    // console.log(e);
+  });
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 500);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
+  ScrollTrigger.create({
+    trigger: "#wrapper",
+    start: "0% 0%",
+    end: "100% 100%",
+    markers: true,
+    id: "scroll",
+    onUpdate: (self) => {
+      // console.log(self);
+    },
+  });
+};
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import { lenisSmoothScrollBar } from "./lenisSmoothScrollBar.js";
+import { changeBgColor } from "./changeBgColor.js";
+
+$(function () {
+  // 부드러운 스크롤 적용
+  lenisSmoothScrollBar();
+  // 배경색 변경
+  changeBgColor();
+});
